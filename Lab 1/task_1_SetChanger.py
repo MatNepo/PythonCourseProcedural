3f8b296f23bd28ba50d4f9b8ea415ae379860d0f
MatNepo <71396245+MatNepo@users.noreply.github.com>
2023-10-10 12:49:49 +0300

    Update task_1_SetChanger.py
    Corrected for version without accessing elements by indexes

--- Lab 1/task_1_SetChanger.py
+++ Lab 1/task_1_SetChanger.py
@@ -7,11 +7,9 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 numbers_without_none = [num for num in numbers if num is not None]
 
 # rms value, round to 2 decimal places
-average = round(sum(numbers_without_none) / (len(numbers)), 2)
+rms_value = sum(numbers_without_none) / len(numbers)
 
-# get index of None
-index_of_none = numbers.index(None)
-# renew result using the index
-numbers[index_of_none] = average
+# renew result using conditions
+numbers = [num if num is not None else rms_value for num in numbers]
 
 print("Измененный список:", numbers)
