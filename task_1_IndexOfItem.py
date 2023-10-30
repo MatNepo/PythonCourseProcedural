f7838e2157069b0a34ff536e95a7024a3f67be82
MatNepo <71396245+MatNepo@users.noreply.github.com>
2023-10-30 14:51:52 +0300

    Lab 3 - pull request added

---
+++ task_1_IndexOfItem.py
@@ -0,0 +1,15 @@
+def index_of_item(total_items, current_item):
+    """Function finds the index from the list of items"""
+    for index, value in enumerate(total_items):
+        if current_item == value:
+            return index
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = index_of_item(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
