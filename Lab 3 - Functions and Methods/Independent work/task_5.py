d43f827a6367201721b40af3fde623c90ada3d13
MatNepo <71396245+MatNepo@users.noreply.github.com>
2023-10-30 14:41:33 +0300

    Delete Lab 3 - Functions and Methods directory

--- Lab 3 - Functions and Methods/Independent work/task_5.py
+++
@@ -1,7 +0,0 @@
-def get_sentences_list(list_):
-    sentences = list_.split(".")
-    strip_sentence = [sentence.strip() for sentence in sentences if sentence.strip()]
-    return strip_sentence
-
-
-print(get_sentences_list("Здесь много разных слов. Возможно и много повторений..."))
